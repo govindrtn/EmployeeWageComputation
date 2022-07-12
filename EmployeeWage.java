a8ed96badcef7ccbbfd330339aafd883351c4043
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-12 12:27:18 +0530

    Added : Calculate wage for multiple Companys

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,43 +2,46 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	
-	public static void calculateTotalWage()
-    {
+    public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+    	
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
-        final int WAGE_PER_HR = 20;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
-
         int totalWage = 0;
         int workingHrs = 0;
-        
-        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
-        {
 
+        System.out.println("Details of " + companyName + " employee");
+        System.out.println("Wage per hour : " + wagePerHr);
+        System.out.println("Maximum working days : " + maxWorkingDays);
+        System.out.println("Maximum working hours : " + maxWorkingHrs);
+        System.out.println();
+
+        for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays
+                && totalWorkingHrs <= maxWorkingHrs; day++, totalWorkingHrs += workingHrs)
+        {
             int empType = (int) (Math.random() * 100) % 3;
             switch (empType)
             {
-            case FULL_TIME:
-                workingHrs = 8;
-                break;
-            case PART_TIME:
-                workingHrs = 4;
-                break;
-            default:
-                workingHrs = 0;
-                break;
+                case FULL_TIME:
+                    workingHrs = 8;
+                    break;
+                case PART_TIME:
+                    workingHrs = 4;
+                    break;
+                default:
+                    workingHrs = 0;
+                    break;
             }
-            int wage = workingHrs * WAGE_PER_HR;
+            int wage = workingHrs * wagePerHr;
             totalWage += wage;
+            
             System.out.println("Employee Daily wages is as per working hour "+workingHrs+ "  Wage  " + wage);
         }
-        System.out.println("Total wage for a month is " + totalWage);
+        System.out.println("Total wage for a month of " + companyName + " employee is " + totalWage + "\n");
     }
-
     public static void main(String args[])
     {
-        calculateTotalWage();
+        calculateTotalWage("Bridgelabs", 40, 15, 200);
+        calculateTotalWage("Dmart", 20, 20, 100);
+        calculateTotalWage("Google", 20, 20, 100);
     }
 }
