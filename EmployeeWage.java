70f65a8d395a892aa6ed25569312a2a5ef8e098b
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-12 08:57:06 +0530

    Added : partTime employee adeed

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,17 +2,23 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	
-	public static void main(String args[]) {
-		
-        final int FULL_TIME = 1;
+	public static void main(String args[])
+    {
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        int empType = (int) (Math.random() * 100) % 2;
-        int workingHours = 0;
         
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours = 0;
+
         if (empType == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is FullTime");
             workingHours = 8;
+        } else if (empType == PART_TIME)
+        {
+            System.out.println("Employee is PartTime");
+            workingHours = 4;
         } else
         {
             System.out.println("Employee is Absent");
@@ -20,5 +26,4 @@ public class EmployeeWage {
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
     }
-	
 }
